107380e5a8dcee65e9c5d2f4dfaf9f06dc825dd2
kingb1rd <kingb1rdx@gmail.com>
2022-06-04 20:06:00 +0200

    feat: navigation between composables + routes

--- app/src/main/java/com/infoechebo/cryptoinfo/MainActivity.kt
+++ app/src/main/java/com/infoechebo/cryptoinfo/MainActivity.kt
@@ -3,6 +3,14 @@ package com.infoechebo.cryptoinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.infoechebo.cryptoinfo.presentation.Screen
+import com.infoechebo.cryptoinfo.presentation.coin_details.components.CoinDetailsScreen
+import com.infoechebo.cryptoinfo.presentation.coin_list.components.CoinListScreen
 import com.infoechebo.cryptoinfo.presentation.ui.theme.CryptoInfoTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +18,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoInfoTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
 
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+
+                        composable(
+                            route = Screen.CoinDetailsScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailsScreen()
+                        }
+                    }
+                }
             }
         }
     }
