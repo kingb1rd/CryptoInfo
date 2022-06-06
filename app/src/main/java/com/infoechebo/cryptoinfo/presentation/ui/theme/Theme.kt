a864bd80dc5be65f26fb5e28abcd1d1741dee1b2
kingb1rd <kingb1rdx@gmail.com>
2022-06-06 12:01:01 +0200

    refactor: theme fix

--- app/src/main/java/com/infoechebo/cryptoinfo/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/infoechebo/cryptoinfo/presentation/ui/theme/Theme.kt
@@ -1,21 +1,33 @@
 package com.infoechebo.cryptoinfo.presentation.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val ColorPalette = lightColors(
+private val DarkColorPalette = darkColors(
     primary = ColorPrimary,
     background = DarkGray,
     onBackground = TextWhite,
     onPrimary = DarkGray
 )
 
+private val LightColorPalette = lightColors(
+    primary = ColorPrimary,
+    background = Color.White,
+    onBackground = MediumGray,
+    onPrimary = DarkGray
+)
+
 @Composable
-fun CryptoInfoTheme(content: @Composable () -> Unit) {
-    val colors = ColorPalette
+fun CryptoInfoTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
 
     val systemUiController = rememberSystemUiController()
 
